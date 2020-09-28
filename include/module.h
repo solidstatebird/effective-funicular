5624c7fee794714073f454151f4edfb99455cc6c
solidstatebird <33707046+solidstatebird@users.noreply.github.com>
2020-09-27 18:53:24 -0700

    update gear ratio

--- include/module.h
+++ include/module.h
@@ -8,6 +8,7 @@
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////////   CONFIG     ///////////////////////////////
 /////////////////////////////////////////////////////////////////////////
+
 const float ANGLE_PID_SAMPLE_TIME = 0.002;
 const float SPEED_PID_SAMPLE_TIME = 0.001;
 
@@ -16,12 +17,12 @@ const float ENCODER_TICKS_PER_REVOLUTION = 28;
 const int MAX_MOTOR_OUTPUT = 255;
 
 const float STEERING_RATIO = (41.0 / 17.0) * (41.0 / 17.0) * (97.0 / 17.0), //motor:module
-            WHEEL_RATIO = STEERING_RATIO * (15.0 / 59.0); //motor:wheel
+            WHEEL_RATIO = STEERING_RATIO * (15.0 / 59.0) * (10.0 / 9.0); //motor:wheel
 
 const float WHEEL_CIRCUMFERENCE_IN = PI * 2.5;
 
-const float SPEED_KP = 20,
-            SPEED_KI = 0,
+const float SPEED_KP = 20.0,
+            SPEED_KI = 0.0,
             SPEED_KD = 0.0,
             ANGLE_KP = 220.0,
             ANGLE_KI = 0.0,
@@ -33,8 +34,8 @@ const int MAGNET_THRESHOLDS[] = {25, 50, 200};
 
 //////////////////////////////// PINS ////////////////////////////////
 
-//           <module 1> <module 2>
-//             m1, m2     m1, m2
+//           <module 1> <module 2> <module 3>
+//             m1, m2     m1, m2     m1, m2
 
 const uint8_t DIRPINS[3][2] = {{21, 20}, {31, 32}, {5, 6}};
 const uint8_t PWMPINS[3][2] = {{23, 22}, {29, 30}, {3, 4}};
@@ -43,13 +44,15 @@ const uint8_t HALLPINS[] = {A15, A14, A16};
 
 //           <module 1>      <module 2>       <module 3>
 //          <m1>    <m2>    <m1>    <m2>     <m1>    <m2>
-//          A,B      A,B    A,B     A,B      A,B     A,B
+//          A,B      A,B    A,B      A,B     A,B      A,B
 
 const uint8_t ENCODERPINS[3][2][2] = {{{19, 18}, {17, 16}}, {{25, 26}, {27, 28}}, {{7, 8}, {9, 10}}};
 
+
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
+
 
 enum ModuleID
 {
